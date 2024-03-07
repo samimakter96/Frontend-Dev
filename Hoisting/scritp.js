f8b8395b4ee605d4d0263f6f668fc7c7a7cca73e
samimakter96 <samimakter9641@gmail.com>
2024-03-07 18:47:51 +0530

    hoistin in js

---
+++ Hoisting/scritp.js
@@ -0,0 +1,21 @@
+// funName();
+// console.log(x);
+// console.log(funName);
+
+// var x = 8;
+
+// function funName() {
+//   console.log('Hello everyone');
+// }
+
+// var funName = () => {
+//   console.log('Hell world');
+// }
+
+// funName();
+
+
+'use strict'
+
+
+
