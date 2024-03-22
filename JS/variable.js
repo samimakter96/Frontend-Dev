bc428f12decb12fa4f4fe8c0b40074ffc3e91cce
samimakter96 <samimakter9641@gmail.com>
2024-03-22 21:43:46 +0530

    updated

---
+++ JS/variable.js
@@ -0,0 +1,22 @@
+let firstName = "Samim";
+let lastName = "Akter";
+let age = 20;
+let isHappy = true;
+
+firstName = 'Ankit'
+
+let a;
+
+const hoursInDay = 24;
+
+let userIntro =
+  "Hi, my name is " +
+  firstName +
+  " " +
+  lastName +
+  ". I am " +
+  age +
+  " " +
+  "years old";
+
+console.log(userIntro);
