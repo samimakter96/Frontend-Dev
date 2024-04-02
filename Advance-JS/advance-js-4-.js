bf523434156609c4bb1a6fa4a04fb803958af03d
samimakter96 <samimakter9641@gmail.com>
2024-04-02 13:45:12 +0530

    prototype higher order function

--- Advance-JS/advance-js-4-.js
+++ Advance-JS/advance-js-4-.js
@@ -71,20 +71,79 @@ const quad = function(num) {
   return num * num * num * num;
 }
 
-const calculate = function(num, logic) {  // calculate is a higher order function
+// const calculate = function(array, logic) {  // calculate is a higher order function
+//   const result = [];
+//   for (let i = 0; i < array.length; i++) {
+//     result.push(logic(array[i]))
+//   }
+//   return result
+// }
+
+// console.log(calculate(num, cube));
+// console.log(num.map(cube)); // map is a function which does the same task what calculate is doing
+
+
+Array.prototype.calculate = function(logic) {  // calculate is a higher order function
   const result = [];
-  for (let i = 0; i < num.length; i++) {
-    result.push(logic(num[i]))
+  for (let i = 0; i < this.length; i++) {
+    result.push(logic(this[i]))
   }
   return result
 }
 
-console.log(calculate(num, cube));
-console.log(calculate(num, square));
-console.log(calculate(num, quad));
+// console.log(num.calculate(cube));
+// console.log(num.map(cube)); // map is a function which does the same task what calculate is doing
+
+// console.log(calculate(num, square));
+// console.log(calculate(num, quad));
 
 // Advantages of functional programming
 
 // 1. Modularity through out the code
 // 2. Reusability
 // 3. Passing function into different function
+
+
+// HTML DOM 
+
+
+// Node 
+
+// document Node 
+// <html> - root Node 
+//     <head> - Element Node 
+//     <bod> - Element Node 
+//       <img> - Element Node src - Attribute Node - text - Text Node
+//       <h1>  - Element Node - text - Text Node 
+
+
+// document.getElementById('demo').innerHTML = 'Samim'
+// document.getElementsByTagName()
+// document.getElementsByClassName()
+
+// Property 
+// Element.innerHTML - Changes the inner html content 
+// Element.attribute - changes the attribute value of that element 
+// Element.style.property - changes the style of an HTML element 
+// Methods 
+// Element.setAttribute(attribute, value) - change the attribute value of an HTML element 
+
+// document.createElement(element)
+// document.removeChild(element);
+// document.appendChild(element);
+// document.replaceChild(new, old)
+// document.write('abcdefg')
+
+// Event handlers 
+// document.getElementById('demo').onclick = function() {}
+
+
+
+// const element = document.getElementsByTagName('p');
+// document.getElementById('demo').innerHTML = element[0].innerHTML;
+
+// const element = document.getElementsByClassName('second');
+// document.getElementById('demo').innerHTML = element[0].innerHTML;
+
+const element = document.querySelectorAll('p.first');
+document.getElementById('demo').innerHTML = element[0].innerHTML;
